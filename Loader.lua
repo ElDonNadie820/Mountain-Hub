9e8638293f66303407555ab01782794cd4e9c37c
ElDonNadie820 <145418989+ElDonNadie820@users.noreply.github.com>
2025-01-20 03:35:15 -0300

    Create Loader.lua

---
+++ Loader.lua
@@ -0,0 +1,7 @@
+if game.GameId == 6360625071 then
+loadstring(game:HttpGet("https://raw.githubusercontent.com/ElDonNadie820/Mountain-Hub/refs/heads/main/MountainHub%20Anime%20Card%20Master.txt", true))()
+elseif game.GameId == 4777817887 then
+loadstring(game:HttpGet("https://raw.githubusercontent.com/ElDonNadie820/Mountain-Hub/refs/heads/main/MountainHub%20Blade%20Ball.txt", true))()
+else
+    game.Players.LocalPlayer:Kick("This Game Is Not Supported Sorry!")
+end
